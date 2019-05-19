b413b022e38c97c244592e81f2fd41af1f9f373f
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-05-19 07:55:56 +0300

    Add API versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      scope module: 'v1', constraints: ApiVersion.new('v1', true) do
+        resources :users
+      end
+    end
+  end
 end
